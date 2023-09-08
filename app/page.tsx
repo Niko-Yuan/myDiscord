8ad81af4ded3ee6e3fffca2c6ac5a78864ce6c95
Niko-Yuan <hairforcenikoyuan@gmail.com>
2023-09-07 19:34:49 -0700

    update feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col">
       <p className="text-3xl font-bold text-indigo-500">
         Hello Discord Clone
       </p>
+      <Button variant="ghost">Click Me</Button>
     </div>
   );
 }
